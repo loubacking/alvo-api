dc6beea4b428e6da4a7966fdf2f732ccd8cae623
Mateus Mourão <mmdfmateus@gmail.com>
2022-06-26 22:36:16 -0300

    add songs repository

---
+++ src/infra/db/repositories/songsRepository.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from "../mongoHelper"
+
+export type Song = {
+  artistName: string,
+  createdAt: string,
+  artistId: string,
+  name: string,
+  lyrics: string,
+  chords: string
+}
+
+export class SongsRepository {
+  getAll = async (): Promise<Song[]> => {
+    let songsCollection = await MongoHelper.getCollection('song');
+
+    return songsCollection
+      .find({})
+      .toArray();
+  }
+
+  search = async (name: string): Promise<Song[]> => {
+    let songsCollection = await MongoHelper.getCollection('song');
+
+    return songsCollection
+      .find({ name })
+      .toArray();
+  }
+}

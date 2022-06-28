31647a17d99a77628d86e47fe0b61d66c019db8b
Mateus Mourão <mmdfmateus@gmail.com>
2022-06-27 21:39:56 -0300

    extract routes to factory

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,23 @@
 import 'dotenv/config'
-import express, { Router } from 'express';
+import express from 'express';
 import { MongoHelper } from './infra/db/mongoHelper';
-import { configSongsRoutes } from './presentation/routes/songsRoutes';
-import { configArtistsRoutes } from './presentation/routes/artistsRoutes';
-import { configAuthRoutes } from './presentation/routes/authRoutes';
 import { setupMiddlewares } from './presentation/middlewares';
+import { setupRoutes } from './presentation/routes';
   
 const app = express();
 
 setupMiddlewares(app);
-
-app.get('/status', (req, res) => res.sendStatus(200));
-
-const router = Router();
-configSongsRoutes(router);
-configArtistsRoutes(router);
-configAuthRoutes(router);
-
-app.use(router);
+setupRoutes(app);
 
 const port = process.env.PORT || 3080;
 
 const mongoUri = process.env.MONGO_DB;
 MongoHelper.connect(mongoUri)
-.then(() => {
-  
-  app.listen(port, () => {
-    console.info(`App listening on port ${port}`);
-  });
-})
-.catch(console.error);
+  .then(() => {
+    
+    app.listen(port, () => {
+      console.info(`App listening on port ${port}`);
+    });
+  })
+  .catch(console.error);
   

cffb73267b52b546ca0dc70561a0ee63eed941a5
Mateus Mourão <mmdfmateus@gmail.com>
2022-07-10 19:45:40 -0300

    add artists routes params on swagger

--- src/presentation/routes/artistsRoutes.ts
+++ src/presentation/routes/artistsRoutes.ts
@@ -9,13 +9,73 @@ const songsRepository = new SongsRepository();
 const controller = new ArtistsController(artistsRepository, songsRepository);
 
 export const configArtistsRoutes = (router: Router) => {
-  router.get('/artists', controller.getArtists);
-  router.get('/artists/:id', controller.getArtistById);
-  router.get('/artists/:id/songs', controller.getArtistSongs);
-  router.get('/searchArtist', controller.searchArtist);
+  router.get('/artists', (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Get a list of artists'
 
-  router.post('/artists', isAuthenticated, controller.createArtist);
+    controller.getArtists(req, res);
+  });
 
-  router.put('/artists', isAuthenticated, controller.editArtist);
+  router.get('/artists/:id', (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Get artist with id provided'
+    /* #swagger.parameters['id'] = {
+      in: 'query',
+      required: 'true'
+    } */
+
+    controller.getArtistById(req, res);
+  });
+
+  router.get('/artists/:id/songs', (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Get songs from the provided artist'
+    /* #swagger.parameters['id'] = {
+      in: 'query',
+      required: 'true'
+    } */
+
+    controller.getArtistSongs(req, res);
+  });
+
+  router.get('/searchArtist', (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Search artists that matches keyword'
+    /* #swagger.parameters['keyword'] = {
+      in: 'query',
+      required: 'true'
+    } */
+
+    controller.searchArtist(req, res);
+  });
+
+  router.post('/artists', isAuthenticated, (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Create new artist'
+    /* #swagger.parameters['body'] = {
+      in: 'body',
+      schema: {
+        $name: 'Ednaldo Pereira',
+        $imageUrl: 'image_link',
+      }
+    } */
+
+    controller.createArtist(req, res);
+  });
+
+  router.put('/artists', isAuthenticated, (req, res) => {
+    // #swagger.tags = ['Artists']
+    // #swagger.description = 'Edit artist data'
+    /* #swagger.parameters['body'] = {
+      in: 'body',
+      schema: {
+        $id: 'artistId',
+        name: 'Ednaldo Pereira',
+        imageUrl: 'image_link',
+      }
+    } */
+
+    controller.editArtist(req, res);
+  });
 
 }
